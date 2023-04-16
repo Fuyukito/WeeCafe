e076a8eb3413913b345ff65deb586f80f5e3b43e
Fuyukito <danilorms.dr@gmail.com>
2023-04-16 09:59:04 +0100

    WebCam

--- main.js
+++ main.js
@@ -9,3 +9,32 @@ function load_window () {
 }
 
 app.whenReady().then(load_window)
+
+var video = document.getElementById("video");
+var startButton = document.getElementById("start-button");
+var stopButton = document.getElementById("stop-button");
+
+var stream;
+
+startButton.addEventListener("click", function() {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function(mediaStream) {
+      stream = mediaStream;
+      video.srcObject = mediaStream;
+      video.play();
+      startButton.disabled = true;
+      stopButton.disabled = false;
+    })
+    .catch(function(err) {
+      console.log("Error accessing webcam: " + err.message);
+    });
+});
+
+stopButton.addEventListener("click", function() {
+  stream.getTracks().forEach(function(track) {
+    track.stop();
+  });
+  video.srcObject = null;
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
